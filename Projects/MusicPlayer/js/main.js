49dc1042a9c849f93eced991fa0e17ee135fc77e
Saleh Riaz Qureshi <salehriazq@gmail.com>
2018-07-29 19:09:53 +0500

    Music Player
    Initial Commit

---
+++ Projects/MusicPlayer/js/main.js
@@ -0,0 +1,20 @@
+$(".btn-play1").click(function(){
+    $("#btnPlay").toggle();
+    $("#btnPause").toggle();
+});
+
+$("#btnPause").click(function(){
+    $("#btnPlay").toggle();
+    $("#btnPause").toggle();
+});
+
+
+$(".btn-play2").click(function(){
+    $("#btnPlay2").toggle();
+    $("#btnPause2").toggle();
+});
+
+$("#btnPause2").click(function(){
+    $("#btnPlay2").toggle();
+    $("#btnPause2").toggle();
+});
