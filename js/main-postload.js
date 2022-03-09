af25e2adfbc94071b850f379afea6a05908867bf
salehriaz <salehriazq@gmail.com>
2022-03-09 15:50:18 +0500

    New Portfolio 2022
    Pushing the new portfolio with updates

--- js/main-postload.js
+++ js/main-postload.js
@@ -1,6 +1,6 @@
 // hide body scroll when WIP overlay
 var wipDisplay = $('.work-in-progress').css('display');
-console.log(wipDisplay);
+// console.log(wipDisplay);
 
 if (wipDisplay == "block") {
     $("body").css("overflow", "hidden");
@@ -22,10 +22,10 @@ function pageRedirect() {
     window.location.replace("playground.html");
 }
 
-$(".clickme").click(function () {
+$(".playground-click").click(function () {
     $(".loader-overlay").css("transform", "translateX(100%)");
-
     setTimeout("pageRedirect()", 3400);
+
 });
 
 //    setTimeout(function() {
@@ -63,7 +63,6 @@ $('a[href^=mailto]').click(function () {
 
 //hover company
 $(".introduction-link").hover(function () {
-    console.log("company");
     $(".company-profile").css("visibility", "visible");
     //        $(".company-profile").css("transform", "translateY(0px)");
 }, function () {
@@ -77,4 +76,55 @@ $(".company-profile").hover(function () {
 }, function () {
     $(".company-profile").css("visibility", "hidden");
     // $(".company-profile").css("transform", "translateY(30px)");
+});
+
+
+// video hover
+$(document).ready(function () {
+    $(".case-card-anchor").mouseenter(function () {
+        $('video', this)[0].play();
+        $('h2', this).addClass("linear-wipe-preactivated");
+    });
+
+    $(".case-card-anchor").mouseleave(function () {
+        $('video', this)[0].pause();
+        //$('video', this)[0].currentTime = 0;
+        $('h2', this).removeClass("linear-wipe-preactivated");
+    });
+});
+
+//weekly card hover
+$(document).ready(function () {
+    $(".weekly-card-anchor").mouseenter(function () {
+        $('h2', this).addClass("linear-wipe-preactivated");
+    });
+
+    $(".weekly-card-anchor").mouseleave(function () {
+        $('h2', this).removeClass("linear-wipe-preactivated");
+    });
+});
+
+
+// show mobile menu
+$('#menuIcon').click(function () {
+    if ($('#menuIcon').is(':checked')) {
+        console.log('checked');
+        $(".mobile-menu-overlay").css('transform', 'scale(300)');
+        $("body").css('overflow', 'hidden');
+        $(".mobile-menu-items").css('display', 'block');
+        setTimeout(
+            function () {
+                $(".mobile-menu-items ul li").css('opacity', '1');
+            }, 500);
+
+    }
+    else {
+        console.log('unchecked');
+        $(".mobile-menu-items ul li").css('opacity', '0');
+        $(".mobile-menu-items").css('display', 'none');
+        $("body").css('overflow', 'auto');
+        $(".mobile-menu-overlay").css('transform', 'scale(1)');
+
+    }
+
 });
