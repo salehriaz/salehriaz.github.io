d3d188c9e8bd93207ca3e20e90415789513a22e0
salehriaz <salehriazq@gmail.com>
2025-02-04 02:27:03 +0500

    Game - For fun
    First commit - Temp / Just for fun and experimentation

--- js/main-postload.js
+++ js/main-postload.js
@@ -143,3 +143,49 @@ $('#menuIcon').click(function () {
     }
 
 });
+
+
+// game script
+counter = 0;
+
+document.addEventListener("DOMContentLoaded", function() {
+    function OTPInput() {
+        const inputs = document.querySelectorAll('#otp > input');
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].addEventListener('input', function() {
+                if (this.value.length > 1) {
+                    this.value = this.value[0]; //    
+                }
+                if (this.value !== '' && i < inputs.length - 1) {
+                    inputs[i + 1].focus(); //   
+                }
+            });
+
+            inputs[i].addEventListener('keydown', function(event) {
+                if (event.key === 'Backspace') {
+                    this.value = '';
+                    if (i > 0) {
+                        inputs[i - 1].focus();   
+                    }
+                }
+            });
+        }
+    }
+
+    OTPInput();
+
+    const validateBtn = document.getElementById('validateBtn');
+    validateBtn.addEventListener('click', function() {
+        let otp = '';
+        document.querySelectorAll('#otp > input').forEach(input => otp += input.value);
+        
+        // if (otp == "12345") {
+        //     alert(`Entered OTP: ${otp}`);  
+        //   }
+        
+        counter = counter + 1;  
+        alert(`Do you think I am stupid? What did I tell you about being cheeky?`);  
+
+        
+    });
+});
